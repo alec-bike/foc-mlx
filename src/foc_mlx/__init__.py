d4381c7e3ca26f78e007916492811c3b37d74bef
Alec Robertson <fencing-suits0i@icloud.com>
2026-08-22 17:20:27 -0700

    Fix code and doc inconsistencies

--- src/foc_mlx/__init__.py
+++ src/foc_mlx/__init__.py
@@ -1,5 +1,15 @@
 """Field Oriented Control core transforms."""
 
 from .core import clarke, duty_cycle, inv_clarke, inv_park, park, svm
+from .sim import control, plot
 
-__all__ = ["clarke", "duty_cycle", "inv_clarke", "inv_park", "park", "svm"]
+__all__ = [
+    "clarke",
+    "control",
+    "duty_cycle",
+    "inv_clarke",
+    "inv_park",
+    "park",
+    "plot",
+    "svm",
+]
