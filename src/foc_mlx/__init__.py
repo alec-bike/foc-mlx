a49944a8702b1e15efa0455c7b4e86a261fcf215
Alec Robertson <41309606+alec-bike@users.noreply.github.com>
2026-08-25 14:45:32 -0700

    Update version to 0.2.0 (#6)
    * Remove sim scripts from __init__.py
    * Update version to 0.2.0

--- src/foc_mlx/__init__.py
+++ src/foc_mlx/__init__.py
@@ -1,15 +1,12 @@
 """Field Oriented Control core transforms."""
 
 from .core import clarke, duty_cycle, inv_clarke, inv_park, park, svm
-from .sim import control, plot
 
 __all__ = [
     "clarke",
-    "control",
     "duty_cycle",
     "inv_clarke",
     "inv_park",
     "park",
-    "plot",
     "svm",
 ]
--- tests/test_foc.py
+++ tests/test_foc.py
@@ -3,7 +3,8 @@
 import pytest
 from mlx.core import array, isclose, pi, sin, sqrt
 
-from foc_mlx import clarke, control, duty_cycle, inv_clarke, inv_park, park, plot, svm
+from foc_mlx import clarke, duty_cycle, inv_clarke, inv_park, park, svm
+from foc_mlx.sim import control, plot
 
 ZERO = array(0)
 ONE = array(1)
